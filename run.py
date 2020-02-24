7ccf188c6b9a8633e5e4b96017667649ab1556a9
rxm7244 <rxm7244@rit.edu>
2020-02-24 15:19:13 -0500

    Basic linear RNN implemented in Torch, training complete

---
+++ run.py
@@ -0,0 +1,11 @@
+"""
+@file run.py
+@author Ryan Missel
+
+Handles taking in a dataset and model and running the training/output on it
+"""
+import torch
+import numpy as np
+import pandas as pd
+
+
